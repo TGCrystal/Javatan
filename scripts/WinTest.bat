6e5036f0c3a86340c2e4e2524125d82940796c94
Trevor Crystal <TGCrystal@users.noreply.github.com>
2021-04-08 01:23:55 -0400

    Added scripts

---
+++ scripts/WinTest.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+cd ..
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.RoadTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.TileTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.TupleTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.BuildingTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.BoardTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.CardTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.DevelopmentCardTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.ResourceCardTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.PlayerTest
+java -cp lib\junit-4.13.jar;lib\hamcrest-core-1.3.jar;src;tests;. org.junit.runner.JUnitCore edu.rpi.cs.csci4960.s21.javatan.GameTest
+cd scripts
