fc86529142d4f572d2a9ac97abc2524431621108
Ruben McWilliams <30909047+rmcwilliams@users.noreply.github.com>
2021-05-01 19:39:04 -0400

    Add files via upload

---
+++ src/edu/rpi/cs/csci4960/s21/javatan/Road.java
@@ -0,0 +1,39 @@
+package app;
+
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.shape.Polygon; 
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import javafx.scene.control.Button;
+import javafx.event.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+public class Road { 
+
+	private Polygon thePolygon;
+	private Group theRoot;
+
+	public Road(Group root, Polygon polygon) {
+		theRoot = root;
+		thePolygon = polygon;
+		theRoot.getChildren().add(polygon);
+	}
+
+	public void makeColor(String color) {
+		if (color.equals("red")) {
+			thePolygon.setFill(javafx.scene.paint.Color.RED);
+		} else if (color.equals("orange")) {
+			thePolygon.setFill(javafx.scene.paint.Color.ORANGE);
+		} else if (color.equals("white")) {
+			thePolygon.setFill(javafx.scene.paint.Color.WHITE);
+		} else if (color.equals("blue")) {
+			thePolygon.setFill(javafx.scene.paint.Color.BLUE);
+		}
+	}
+
+	
+}
