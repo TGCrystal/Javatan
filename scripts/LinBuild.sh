0d67d57c7cb9a3b24dc95d035d00668248f4dcc4
TGCrystal <TGCrystal@users.noreply.github.com>
2021-05-02 22:43:59 -0400

    fixed linux scripts

--- scripts/LinBuild.sh
+++ scripts/LinBuild.sh
@@ -1,7 +1,6 @@
-@ECHO OFF
 cd ..
-set PATH_TO_FX="javafx-sdk-11.0.2/lib"
-javac --module-path %PATH_TO_FX% --add-modules javafx.controls -encoding UTF-8 src/edu/rpi/cs/csci4960/s21/javatan/*.java
-javac --module-path %PATH_TO_FX% --add-modules javafx.controls -encoding UTF-8 -cp lib/junit-4.13.jar:src:. tests/edu/rpi/cs/csci4960/s21/javatan/*.java
-javadoc --module-path %PATH_TO_FX% --add-modules javafx.controls edu.rpi.cs.csci4960.s21.javatan -encoding UTF-8 -cp src:. -d docs
+export PATH_TO_FX=javafx-sdk-11.0.2/lib
+javac --module-path $PATH_TO_FX --add-modules javafx.controls -encoding UTF-8 src/edu/rpi/cs/csci4960/s21/javatan/*.java
+javac --module-path $PATH_TO_FX --add-modules javafx.controls -encoding UTF-8 -cp lib/junit-4.13.jar:src:. tests/edu/rpi/cs/csci4960/s21/javatan/*.java
+javadoc --module-path $PATH_TO_FX --add-modules javafx.controls edu.rpi.cs.csci4960.s21.javatan -encoding UTF-8 -cp src:. -d docs
 cd scripts
